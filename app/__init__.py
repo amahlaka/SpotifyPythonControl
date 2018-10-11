4778934beed9ca47509e057c970227171f03743f
Arttu <arttu.mahlakaarto+user@gmail.com>
2018-10-11 20:40:42 +0300

    Changed comment to better one

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,6 @@ from flask import Flask
 
 
 app = Flask(__name__)
-app.secret_key = ''  # Generate a secret key here
+app.secret_key = ''  # Paste the generated key here
 
 from app import authenticater
